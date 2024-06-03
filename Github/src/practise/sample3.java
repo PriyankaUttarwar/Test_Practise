27bf81e0c11304108698c8217d9423aaad9fa7c5
PriyankaUttarwar <uttarwar.priyanka2@gmail.com>
2024-06-03 16:26:23 -0700

    commit

--- Github/src/practise/sample3.java
+++ Github/src/practise/sample3.java
@@ -5,7 +5,8 @@ public class sample3 {
 	public static void main(String[] args) {
 	
 		System.out.println("sample3");
-		System.out.println("Added new line in branch4 merge conflict");
+		int a;
+		a=9;
 	}
 
 }
